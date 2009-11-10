be4691650db645c276e633b0508bd410d7722a1e
Nick Stielau <nick.stielau@gmail.com>
2009-11-10 14:08:58 -0800

    Migrating to template

--- lib/snap.rb
+++ lib/snap.rb
@@ -1,18 +1,1 @@
-require 'rubygems'
-require 'sinatra'
-
-module Snap
-  get '/' do
-    "<h2>ooooh Snap!</h2> <ul>#{get_files.map{|f| "<li>#{f}</li>"}.join()}</ul> are the files in #{get_dir}"
-  end
-  
-  def get_dir
-    File.dirname(__FILE__)
-  end
-  
-  def get_files
-    Dir.glob("#{get_dir}/**")
-  end
-  
-end
-
+require 'snap/main'
--- lib/snap/main.rb
+++ lib/snap/main.rb
@@ -1,9 +1,25 @@
 require 'rubygems'
 require 'sinatra'
+require 'erb'
 
 module Snap
+  set :views, File.dirname(__FILE__) + '/views'
+  
   get '/' do
-    'Hello world!'
+    erb :index
   end
+  
+  get '/__snap__/:image.:format' do
+    filename = File.dirname(__FILE__) + "/images/#{params[:image]}.#{params[:format]}"
+    content_type params[:format]
+    send_file filename
+  end
+  
+  def get_dir
+    File.dirname(__FILE__)
+  end
+  
+  def get_files
+    Dir.glob("#{get_dir}/**")
+  end  
 end
-
